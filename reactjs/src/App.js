07485fa4f548c3d3e4fbc4252581fa84f99096bf
Yiota Ziaggou <yiotaz@transifex.com>
2020-09-14 10:46:03 +0300

    Style the reactjs sandbox
    This commit adds styling to the reactjs sandbox

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,86 +1,95 @@
-import React, {useState} from 'react';
+import React, { useState } from "react";
 
-import {tx} from '@transifex/native';
-import {T, UT, LanguagePicker} from '@transifex/react';
+import { tx } from "@transifex/native";
+import { T, UT, LanguagePicker } from "@transifex/react";
 
 tx.init({
-  sourceLocale: 'en',
-  token: '1/066926bd75f0d9e52fce00c2208ac791ca0cd2c1',
+  sourceLocale: "en",
+  token: "1/066926bd75f0d9e52fce00c2208ac791ca0cd2c1",
 });
 
 function App() {
-  const [string, setString] = useState('John Doe');
+  const [string, setString] = useState("John Doe");
   const [number, setNumber] = useState(1);
-  const [choice, setChoice] = useState('female');
+  const [choice, setChoice] = useState("female");
 
   return (
-    <table>
-      <tbody>
-        <tr>
-          <th>Static content:</th>
-          <td></td>
-          <td><T _str="Hello world" /></td>
-        </tr>
-        <tr>
-          <th>Dynamic content:</th>
-          <td>
-            <input
-              value={string}
-              onChange={(e) => setString(e.target.value)} />
-          </td>
-          <td><T _str="Hello {username}" username={string} /></td>
-        </tr>
-        <tr>
-          <th>Pluralized:</th>
-          <td>
+    <div className="container">
+      <div className="header">
+        <h1>Transifex Native playground</h1>
+        <div className="picker">
+          <LanguagePicker />
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          <h3>Static content</h3>
+          <div>
+            <T _str="Hello world" />
+          </div>
+        </div>
+        <div className="column">
+          <h3>Dynamic content</h3>
+          <div>
+            <input value={string} onChange={(e) => setString(e.target.value)} />
+            <p>
+              <T _str="Hello {username}" username={string} />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          <h3>Pluralized</h3>
+          <div>
             <input
               type="number"
               value={number}
-              onChange={(e) => setNumber(e.target.value)} />
-          </td>
-          <td>
-            <T
-              _str="{cnt, plural, one {You have # message} other {You have # messages}}"
-              cnt={number} />
-          </td>
-        </tr>
-        <tr>
-          <th>Choice:</th>
-          <td>
+              onChange={(e) => setNumber(e.target.value)}
+            />
+            <p>
+              <T
+                _str="{cnt, plural, one {You have # message} other {You have # messages}}"
+                cnt={number}
+              />
+            </p>
+          </div>
+        </div>
+        <div className="column">
+          <h3>Choice</h3>
+          <div>
             <select value={choice} onChange={(e) => setChoice(e.target.value)}>
               <option value="male">Male</option>
               <option value="female">Female</option>
               <option value="other">Other</option>
             </select>
-          </td>
-          <td>
-            <T
-              _str="{username} is a {gender, select, male {boy} female {girl} other {person}}"
-              gender={choice}
-              username={string} />
-          </td>
-        </tr>
-        <tr>
-          <th>Raw HTML:</th>
-          <td>
-            <input
-              value={string}
-              onChange={(e) => setString(e.target.value)} />
-          </td>
-          <td>
-            <UT
-              _str="<em style=&quot;color:red&quot;>Hello </em><strong style=&quot;color:blue&quot;>{username}</strong>"
-              _inline
-              username={string} />
-          </td>
-        </tr>
-        <tr>
-          <td><LanguagePicker /></td>
-          <td colSpan={2}></td>
-        </tr>
-      </tbody>
-    </table>
-  )
+            <p>
+              <T
+                _str="{username} is a {gender, select, male {boy} female {girl} other {person}}"
+                gender={choice}
+                username={string}
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <div className="column">
+          <h3>Raw HTML</h3>
+          <div>
+            <input value={string} onChange={(e) => setString(e.target.value)} />
+            <p>
+              <UT
+                _str='<em style="color:red">Hello </em><strong style="color:blue">{username}</strong>'
+                _inline
+                username={string}
+              />
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
