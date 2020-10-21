c089338ae63d4073044308385d4e2b772065c409
Konstantinos Bairaktaris <kbairak@transifex.com>
2020-10-21 15:12:33 +0300

    Add urwid sample application

---
+++ python-tui/run.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+if [ ! -d venv ]; then
+  echo Creating virtual environment \'venv\'
+  python -m venv venv
+fi;
+
+source venv/bin/activate
+
+if [ ! -d venv/lib/python*/site-packages/urwid ]; then
+  pip install -r requirements.txt
+fi
+
+python main.py
